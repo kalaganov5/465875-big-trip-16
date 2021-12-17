ffa1656e2f1374ef5e0b553c18d73de80ed95697
kalaganov5 <kalaganov5@gmail.com>
2021-12-17 19:12:12 +0300

    Добавлена функция replace для удобства замены элементов

--- src/utils/render.js
+++ src/utils/render.js
@@ -47,3 +47,25 @@ export const renderElement = (container, element, place) => {
       break;
   }
 };
+
+/**
+ *
+ * @param {*} newElement элемент на который будет заменён
+ * @param {*} oldElement элемент который будет заменён
+ */
+export const replace = (newElement, oldElement) => {
+  if (newElement === null || oldElement === null) {
+    throw new Error('Can\'t replace unexisting elements');
+  }
+
+  const newChild = newElement instanceof AbstractView ? newElement.element : newElement;
+  const oldChild = oldElement instanceof AbstractView ? oldElement.element : oldElement;
+  const parent = oldChild.parentElement;
+
+  if (parent === null) {
+    throw new Error('Parent element doesn\'t exist');
+  }
+
+  parent.replaceChild(newChild, oldChild);
+};
+
