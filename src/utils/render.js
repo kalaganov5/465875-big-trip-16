d1c19a47855e14f2554208d548686e7f5b2c112f
kalaganov5 <kalaganov5@gmail.com>
2021-12-17 14:55:02 +0300

    Научим функцию render самому вызавать геттер

---
+++ src/utils/render.js
@@ -0,0 +1,49 @@
+import AbstractView from '../view/abstract-view.js';
+
+export const RenderPosition = {
+  BEFOREBEGIN: 'beforebegin',
+  AFTERBEGIN: 'afterbegin',
+  BEFOREEND: 'beforeend',
+  AFTEREND: 'afterend',
+};
+
+/**
+ *
+ * @param {string} template HTML в виде строки, должен иметь общую обёртку
+ * @returns возвращаем этот DOM-элемент
+ */
+export const createElement = (template) => {
+  // создаём пустой div-блок
+  const newElement = document.createElement('div');
+  // берём HTML в виде строки и вкладываем в этот div-блок, превращая в DOM-элемент
+  newElement.innerHTML = template;
+  return newElement.firstChild;
+};
+// Единственный нюанс, что HTML в строке должен иметь общую обёртку,
+// то есть быть чем-то вроде <nav><a>Link 1</a><a>Link 2</a></nav>,
+// а не просто <a>Link 1</a><a>Link 2</a>
+
+/**
+ *
+ * @param {object} container в который поставим разметку
+ * @param {object} element разметка/верстка
+ * @param {string} place положение в начало или в конец
+ */
+export const renderElement = (container, element, place) => {
+  const parentContainer = container instanceof AbstractView ? container.element : container;
+  const childElement = element instanceof AbstractView ? element.element : element;
+  switch (place) {
+    case RenderPosition.BEFOREBEGIN:
+      parentContainer.before(childElement);
+      break;
+    case RenderPosition.AFTERBEGIN:
+      parentContainer.prepend(childElement);
+      break;
+    case RenderPosition.BEFOREEND:
+      parentContainer.append(childElement);
+      break;
+    case RenderPosition.AFTEREND:
+      parentContainer.after(childElement);
+      break;
+  }
+};
