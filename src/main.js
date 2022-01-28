59a68525fca9a611633849ca9843d0cc189f3195
kalaganov5 <kalaganov5@gmail.com>
2022-01-28 15:49:23 +0300

    Рефакторинг сортировки в main

--- src/main.js
+++ src/main.js
@@ -4,12 +4,10 @@ import FilterTripPointsModel from './model/filter-trip-point-model.js';
 import FilterTripPointPresenter from './presenter/filter-trip-point-presenter.js';
 
 import {getRoutePoint} from './mock/get-route-point.js';
-import {sortDayAscending} from './utils/common.js';
 
 // mock
 const TRIP_POINT_COUNT = 20;
 const tripPoints = Array.from({length: TRIP_POINT_COUNT}, getRoutePoint);
-tripPoints.sort(sortDayAscending);
 // mock
 
 const filterTripPointsModel = new FilterTripPointsModel();
