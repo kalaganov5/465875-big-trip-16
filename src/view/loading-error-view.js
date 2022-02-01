0e8d7c8305614e2ca378e30da9d25c2d6e26c926
kalaganov5 <kalaganov5@gmail.com>
2022-02-01 16:41:42 +0300

    Добавлен эффект shake

--- src/view/loading-error-view.js
+++ src/view/loading-error-view.js
@@ -2,6 +2,6 @@ import AbstractView from './abstract-view.js';
 
 export default class LoadingErrorView extends AbstractView {
   get template() {
-    return '<p class="trip-events__msg">Loading Error:</br>Try reloading the page or check back later</p>';
+    return '<p class="trip-events__msg shake">Loading Error:</br>Try reloading the page or check back later</p>';
   }
 }
